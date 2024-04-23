8d5100b2ac892bebdbd44b2776eb93f7205cd97e
Reyhan Afif Fahri <126700421+Reyhan-Code@users.noreply.github.com>
2024-04-23 15:55:58 +0700

    push

---
+++ app/src/main/java/com/dicoding/likesapp/MainActivity.kt
@@ -0,0 +1,188 @@
+package com.dicoding.likesapp
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.Region
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.dicoding.likesapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val message = "Apakah kamu suka bermain?"
+
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
+    private val halfOfWidth = (mBitmap.width/2).toFloat()
+    private val halfOfHeight = (mBitmap.height/2).toFloat()
+
+    private val left = 150F
+    private val top = 250F
+    private val right = mBitmap.width - left
+    private val bottom = mBitmap.height.toFloat() - 50F
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.setImageBitmap(mBitmap)
+        showText()
+
+        // Tombol Suka
+        binding.like.setOnClickListener {
+            showEars()
+            showFace()
+            showMouth(true)
+            showEyes()
+            showNose()
+            showHair()
+
+        }
+
+        // Tombol Tidak suka
+        binding.dislike.setOnClickListener {
+            showEars()
+            showFace()
+            showMouth(false)
+            showEyes()
+            showNose()
+            showHair()
+        }
+    }
+
+    // Menggambar wajah
+    private fun showFace() {
+        val face = RectF(left, top, right, bottom)
+
+        // object pertama dengan sudut 90 derajat
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        // object pertama dengan sudut 270 derajat
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
+    }
+
+    // Menggambar mata
+    private fun showEyes() {
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        mCanvas.drawCircle(halfOfWidth - 100F, halfOfHeight - 10F, 50F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 100F, halfOfHeight - 10F, 50F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+        mCanvas.drawCircle(halfOfWidth - 120F, halfOfHeight - 20F, 15F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 80F, halfOfHeight - 20F, 15F, mPaint)
+    }
+
+    // Menggambar Mulut
+    private fun showMouth(isHappy: Boolean) {
+
+        when (isHappy) {
+
+            // Ketika kondisinya positif (true), maka muncul bibir tersenyum
+            true -> {
+                mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+                val lip = RectF(halfOfWidth - 200F, halfOfHeight - 100F, halfOfWidth + 200F, halfOfHeight + 400F)
+                mCanvas.drawArc(lip, 25F, 130F, false, mPaint)
+
+                mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+                val mouth = RectF(halfOfWidth - 180F, halfOfHeight, halfOfWidth + 180F, halfOfHeight + 380F)
+                mCanvas.drawArc(mouth, 25F, 130F, false, mPaint)
+
+            }
+
+            //Sebaliknya ketika kondisi negatif (false), akan muncul bibir cemberut alias sedih.
+            false -> {
+                mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+                val lip = RectF(halfOfWidth - 200F, halfOfHeight + 250F, halfOfWidth + 200F, halfOfHeight + 350F)
+                mCanvas.drawArc(lip, 0F, -180F, false, mPaint)
+
+                mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+                val mouth = RectF(halfOfWidth - 180F, halfOfHeight + 260F, halfOfWidth + 180F, halfOfHeight + 330F)
+                mCanvas.drawArc(mouth, 0F, -180F, false, mPaint)
+            }
+        }
+    }
+
+    // Menggambar Hidung
+    private fun showNose() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        mCanvas.drawCircle(halfOfWidth - 40F, halfOfHeight + 140F, 15F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 40F, halfOfHeight + 140F, 15F, mPaint)
+    }
+
+    // Menggambar Telinga
+    private fun showEars() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_left_hair, null)
+        mCanvas.drawCircle(halfOfWidth - 300F, halfOfHeight - 100F, 100F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawCircle(halfOfWidth + 300F, halfOfHeight - 100F, 100F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.red_ear, null)
+        mCanvas.drawCircle(halfOfWidth - 300F, halfOfHeight - 100F, 60F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 300F, halfOfHeight - 100F, 60F, mPaint)
+    }
+
+    // Menggambar Rambut
+    private fun showHair() {
+
+        // Menyimpan pengaturan canvas saat ini.
+        mCanvas.save()
+
+
+        // Membuat kerangka object yang akan dipotong.
+        val path = Path()
+
+        path.addCircle(halfOfWidth - 100F,halfOfHeight - 10F, 150F, Path.Direction.CCW)
+        path.addCircle(halfOfWidth + 100F,halfOfHeight - 10F, 150F, Path.Direction.CCW)
+
+        val mouth = RectF(halfOfWidth - 250F, halfOfHeight, halfOfWidth + 250F, halfOfHeight + 500F)
+        path.addOval(mouth, Path.Direction.CCW)
+
+
+        // Memotong object ke dalam Canvas.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            mCanvas.clipPath(path, Region.Op.DIFFERENCE)
+        } else {
+            mCanvas.clipOutPath(path)
+        }
+
+        // Object yang tampil akan terpotong berdasarkan path yang telah diatur.
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_left_hair, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
+
+        // Mengembalikan posisi Canvas sebelum ada ditambahkan clip.
+        mCanvas.restore()
+    }
+
+    private fun showText() {
+        val mPaintText =  Paint(Paint.FAKE_BOLD_TEXT_FLAG).apply {
+            textSize = 50F
+            color = ResourcesCompat.getColor(resources, R.color.black , null)
+        }
+
+        val mBounds = Rect()
+        mPaintText.getTextBounds(message, 0, message.length, mBounds)
+
+        val x: Float = halfOfWidth - mBounds.centerX()
+        val y = 50F
+        mCanvas.drawText(message, x, y, mPaintText)
+    }
+}
